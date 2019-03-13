5f976887e7b3b16ad971fb76ce71ca30779af8ff
Carlos R. Guerra <cguerramain@gmail.com>
2019-03-13 16:01:44 -0400

    Added scripts for setting up the tables for the server.

---
+++ print_tables.sql
@@ -0,0 +1,9 @@
+-- Outputs all the rows from each table
+
+SELECT * FROM locations;
+SELECT * FROM student;
+SELECT * FROM edges;
+SELECT * FROM restaurant;
+SELECT * FROM schedule;
+SELECT * FROM study_time;
+
