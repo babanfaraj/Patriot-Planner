4cf269793f58283a636d48fa5a5edc791aeb1e3a
Baban Faraj <baban_faraj@Hotmail.com>
2019-04-07 15:16:33 -0400

    Added Skeleton class of DBConnectionAll.py

---
+++ skeleton_classes/DBConnecitonAll.py
@@ -0,0 +1,25 @@
+class DBConnectionAll:
+    def __init__(self, _conn):
+        self._conn = _conn
+    def create_user(self,student_info):
+        return False
+    def add_class(self,student,class_time):
+        return False
+    def are_valid_credentials(self,email,password,):
+        return False
+    def get_user(self,email):
+        return 0
+    def get_all_users(self):
+        return 0
+    def get_location(self,location):
+        return 0
+    def get_all_locations(self):
+        return 0
+    def get_edges(self,location_name):
+        return 0
+    def get_graph(self):
+        return 0
+    def get_all_restaraunt(self):
+        return 0
+
+
