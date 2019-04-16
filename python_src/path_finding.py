08338031f4062295818627cace02ac5d0b9f9351
babanfaraj <baban_faraj@Hotmail.com>
2019-04-15 20:35:03 -0400

    Empty get_best_path function added with comments

---
+++ python_src/path_finding.py
@@ -0,0 +1,16 @@
+import heapq
+
+def get_best_path(graph,edgeCosts,start,end):
+    '''
+    Returns a set of predecssor nodes and minimum distance from start to end
+    :param graph: Adjacent List
+    :param edgeCosts: List of edges and its costs
+    :param start: Starting node
+    :param end:   Ending Node
+    :return:
+    '''
+
+
+
+if __name__=='__main__':
+    print("Hello")
