cc717ce690c911a385666ec054e9f2060f008bd1
Alex Smith <asmit27@gmu.edu>
2019-04-16 20:07:47 -0400

    Updated build_schedule (still in rudimentary testing)

--- python_src/build_schedule.py
+++ python_src/build_schedule.py
@@ -1,6 +1,14 @@
-from python_src.db_connection import get_graph
+from python_src.models import *
+from python_src.db_connection import *
 
-def build_schedule(num_hours, student_id):
-    return student_id
 
+def build_schedule(email):
+  cur_student = get_student(email)
+  student_classes = cur_student.all_classes()
+  print(Location.query.all())
+  print("\n")
+  print(student_classes)
+  return email
+
+build_schedule("hello")
 
