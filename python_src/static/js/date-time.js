5e0bba5c52001b95fc984d3049d462bac585399c
Maarvi Memon <Maarvi@Maarvis-MacBook-Pro.local>
2019-04-20 21:50:38 -0400

    Login Page

--- python_src/static/js/date-time.js
+++ python_src/static/js/date-time.js
@@ -1,7 +1,8 @@
 window.onload =
     function () {
+    var weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     var today = new Date();
-    var date = (today.getMonth()+1)+'/'+today.getDate()+'/'+today.getFullYear();
+    var date = weekday[today.getDay()]+ " " + (today.getMonth()+1)+'/'+today.getDate()+'/'+today.getFullYear();
         document.getElementById("time-text").innerHTML=date;
     }
 
