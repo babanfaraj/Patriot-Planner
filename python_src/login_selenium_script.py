e7aa451555e9514ac77cf57acd5175fc90d06a81
babanfaraj <baban_faraj@Hotmail.com>
2019-04-28 15:27:53 -0400

    Updated login_selenium_script.py to include automated login but missing button name for logging in

---
+++ python_src/login_selenium_script.py
@@ -0,0 +1,9 @@
+from selenium import webdriver
+
+driver = webdriver.Chrome()
+
+driver.set_page_load_timeout(10)
+driver.get("http://127.0.0.1:5000/")
+driver.find_element_by_name("email").send_keys("bfaraj@masonlive.gmu.edu")
+driver.find_element_by_name("password").send_keys("password")
+driver.find_element_by_name("")
