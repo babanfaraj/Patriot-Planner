0e132d115fb8654634f168f279fa61c4ab000630
babanfaraj <baban_faraj@Hotmail.com>
2019-04-28 16:16:17 -0400

    Added more test cases to login_selenium_script.py

--- python_src/login_selenium_script.py
+++ python_src/login_selenium_script.py
@@ -4,10 +4,31 @@ import time
 driver = webdriver.Chrome("C:/Users/Baban Faraj/Documents/College/CS321/project/drivers/chromedriver.exe")
 #driver = webdriver.Chrome("project/drivers/chromedriver.exe")
 
+#Test One: Login works
 driver.set_page_load_timeout(10)
 driver.get("http://127.0.0.1:5000/")
 driver.find_element_by_name("email").send_keys("bfaraj@masonlive.gmu.edu")
 driver.find_element_by_name("password").send_keys("password")
-driver.find_element_by_name("login").click()
-time.sleep(4)
+#driver.find_element_by_name("login").click()
+
+#Test Two: Username and Password invalid
+driver.get("http://127.0.0.1:5000/")
+driver.find_element_by_name("email").send_keys("asdf@masonlive.gmu.edu")
+driver.find_element_by_name("password").send_keys("adsf")
+#driver.find_element_by_name("login").click()
+time.sleep(1)
+
+#Test Three: Username valid but password invalid
+driver.get("http://127.0.0.1:5000/")
+driver.find_element_by_name("email").send_keys("bfaraj@masonlive.gmu.edu")
+driver.find_element_by_name("password").send_keys("babaniscool")
+#driver.find_element_by_name("login").click()
+time.sleep(1)
+
+#Test Three: Empty Credentials
+driver.get("http://127.0.0.1:5000/")
+driver.find_element_by_name("email").send_keys("")
+driver.find_element_by_name("password").send_keys("")
+#driver.find_element_by_name("login").click()
+time.sleep(1)
 driver.quit()
