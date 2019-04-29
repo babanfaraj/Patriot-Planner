48797107ded3d5a19f096262a40f8cb27a0a6b1a
babanfaraj <baban_faraj@Hotmail.com>
2019-04-28 20:27:20 -0400

    Added a second test case to test what happens if you update a user's password with the same password

--- python_src/setting_selenium_script.py
+++ python_src/setting_selenium_script.py
@@ -6,7 +6,7 @@ driver = webdriver.Chrome("C:/Users/Baban Faraj/Documents/College/CS321/project/
 
 #Login
 #driver.set_page_load_timeout(10)
-driver.get("http://127.0.0.1:5000/")
+driver.get("http://127.0.0.1:5000/index")
 driver.find_element_by_name("email").send_keys("bfaraj@masonlive.gmu.edu")
 driver.find_element_by_name("password").send_keys("password")
 driver.find_element_by_name("login").click()
@@ -19,4 +19,10 @@ driver.find_element_by_name("confirm_new_password").send_keys("password1")
 driver.find_element_by_name("submit").click()
 time.sleep(1)
 
+#Test Two: Updating Password with same previous password
+driver.get("http://127.0.0.1:5000/settings")
+driver.find_element_by_name("new_password").send_keys("password1")
+driver.find_element_by_name("confirm_new_password").send_keys("password1")
+driver.find_element_by_name("submit").click()
+time.sleep(1)
 
